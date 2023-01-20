15f9bd7624b7d2d04c673532200179e1eef356fc
ArtChaos189 <tvolkiska967@gmail.com>
2023-01-20 11:13:44 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
+
+import { Modal } from "./components/Modal";
+
 import "./index.scss";
 
-const Modal = ({ open, setOpen, children }) => {
-  return (
-    <div className={`overlay ${open ? "animated" : "show"}`}>
-      <div className="modal">
-        <svg onClick={() => setOpen(true)} height="200" viewBox="0 0 200 200" width="200">
-          <title />
-          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-        </svg>
-        {children}
-      </div>
-    </div>
-  );
-};
-
-function App() {
+export const App = () => {
   const [open, setOpen] = useState(true);
 
   return (
@@ -24,10 +13,8 @@ function App() {
         ✨ Открыть окно
       </button>
       <Modal open={open} setOpen={setOpen}>
-        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="logo" />
       </Modal>
     </div>
   );
-}
-
-export default App;
+};
